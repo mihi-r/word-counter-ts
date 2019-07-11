f027087aa190e936f8a9f3d5380e08d054e0ac0c
patel3mh <patel3mh@mail.uc.edu>
2019-07-10 21:21:16 -0400

    Added initial type annotations

---
+++ scripts/main.ts
@@ -0,0 +1,25 @@
+import parseInput from './parser.js';
+
+const intialInput: HTMLTextAreaElement = document.querySelector('.initial-input');
+const wordCount: HTMLParagraphElement = document.querySelector('.word-count');
+const countButton: HTMLButtonElement = document.querySelector('.count-btn');
+const parsedInput: HTMLDivElement = document.querySelector('.parsed-input');
+const specificWord: HTMLInputElement = document.querySelector('.specific-word');
+
+countButton.onclick = function countButtonOnClicked() {
+  while (parsedInput.children.length > 1) {
+    const parsedInputUL = <Element>parsedInput.lastChild;
+    parsedInputUL.remove();
+  }
+
+  let preprocessedInput: Array<string> = intialInput.value.trim().toLowerCase().replace(/(\.|,|!|\?|\(|\)|'|")/gm, '').split(' ');
+  preprocessedInput = preprocessedInput.filter(word => word !== '');
+
+  parseInput(preprocessedInput, parsedInput, specificWord.value.toLowerCase());
+
+  if (specificWord.value) {
+    preprocessedInput = preprocessedInput.filter(word => word === specificWord.value.toLowerCase());
+  }
+
+  wordCount.textContent = `Word count: ${preprocessedInput.length}`;
+};
---
+++ scripts/parser.ts
@@ -0,0 +1,15 @@
+export default function parseInput(input: Array<string>, parseContainer: HTMLDivElement, wordToHighlight = '') {
+  const ulNode: HTMLUListElement = document.createElement('ul');
+
+  input.forEach((word) => {
+    const liNode: HTMLLIElement = document.createElement('li');
+    liNode.textContent = word;
+
+    if (wordToHighlight === word) {
+      liNode.classList.add('word-found');
+    }
+    ulNode.appendChild(liNode);
+  });
+
+  parseContainer.appendChild(ulNode);
+}
